bf83d5a71755183ff4227325ebc3b5acf67fddcc
April25 <1294844209@qq.com>
2020-11-30 16:11:03 +0800

    123

---
+++ src/main/java/cn/demo/controller/DockerController.java
@@ -0,0 +1,25 @@
+package cn.demo.controller;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ * <功能描述>
+ *
+ * @author xuli1294844209@qq.com
+ * @createTime 2020/11/19
+ **/
+@RestController
+public class DockerController {
+    @GetMapping("/")
+    public String show(){
+        StringBuilder stringBuilder=new StringBuilder();
+        stringBuilder.append("Docker success");
+        //new DataSourceProperties().
+        return stringBuilder.toString();
+    }
+}
